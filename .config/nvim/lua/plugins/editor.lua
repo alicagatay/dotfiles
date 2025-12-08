559a32b8ee1cc24ec57fb587b23fa0ac7495491d
Ali Cagatay <aliccagatay@gmail.com>
2025-12-08 16:01:41 +0000

    feat: Add GitHub Copilot and Copilot Chat integration with keybindings

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -56,4 +56,70 @@ return {
       },
     },
   },
+
+  -- GitHub Copilot inline suggestions
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      -- Don't use <Tab> for Copilot so it doesn't conflict with nvim-cmp
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+
+      -- Accept Copilot suggestion with <C-l> in insert mode
+      vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        silent = true,
+        desc = "Accept Copilot suggestion",
+      })
+
+      -- Example: enable Copilot for all filetypes (customize if you like)
+      -- vim.g.copilot_filetypes = { ["*"] = true }
+    end,
+  },
+
+  -- GitHub Copilot Chat
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    cmd = {
+      "CopilotChat",
+      "CopilotChatOpen",
+      "CopilotChatToggle",
+      "CopilotChatExplain",
+      "CopilotChatReview",
+      "CopilotChatFix",
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- On macOS/Linux this builds optional tiktoken support (safe to skip if it fails)
+    build = "make tiktoken",
+    opts = {
+      model = "gpt-4.1",         -- AI model to use
+      temperature = 0.1,          -- Lower = focused, higher = creative
+      window = {
+        layout = "vertical",    -- 'vertical', 'horizontal', 'float'
+        width = 0.3,             -- 50% of screen width
+      },
+      auto_insert_mode = true,   -- Enter insert mode when opening
+    },
+    keys = {
+      {
+        "<leader>cc",
+        "<cmd>CopilotChatToggle<CR>",
+        mode = { "n", "v" },
+        desc = "CopilotChat - Toggle chat",
+      },
+      {
+        "<leader>ce",
+        "<cmd>CopilotChatExplain<CR>",
+        mode = { "n", "v" },
+        desc = "CopilotChat - Explain selection",
+      },
+      {
+        "<leader>cr",
+        "<cmd>CopilotChatReview<CR>",
+        mode = { "n", "v" },
+        desc = "CopilotChat - Review selection",
+      },
+    },
+  },
 }
